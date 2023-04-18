f7c0139b794a3498640f09446343ac67c6248577
Benjamin Bureau <benjaminmbureau@gmail.com>
2023-04-18 13:54:49 +0200

    Updates Skills + projets+ a propos

--- index.js
+++ index.js
@@ -20,3 +20,11 @@ const linkAction = () =>{
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(e=> e.addEventListener('click', linkAction))
+
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                        : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
