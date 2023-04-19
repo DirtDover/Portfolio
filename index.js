8cbfb7a2be35d94fbe15ba88f4d132a89779f92f
Benjamin Bureau <benjaminmbureau@gmail.com>
2023-04-19 15:04:52 +0200

    Responsives

--- index.js
+++ index.js
@@ -28,3 +28,53 @@ const blurHeader = () =>{
                         : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
+
+const contactForm = document.getElementById('contact-form'),
+    contactMessage= document.getElementById('contact-message')
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+    emailjs.sendForm('service_hunbebf','template_m1mk4mq', '#contact-form', 'HcPs1MeekaHisKKK2')
+      .then(()=>{
+        contactMessage.textContent = 'Message envoyé !'
+
+        setTimeout(()=>{
+            contactMessage.textContent=''
+        },3000)
+
+        contactForm.reset()
+      }, () =>{
+        contactMessage.textContent = 'Message non envoyé (service error)'
+      })
+}
+
+contactForm.addEventListener('submit', sendEmail)
+
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                         :scrollUp.classList.remove('show-scroll')   
+}
+
+window.addEventListener('scroll', scrollUp)
+
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId+ ']')
+   
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            sectionClass.classList.add('active-link')
+        }else{
+            sectionClass.classList.remove('active-link')
+        }
+            })
+}
+
+window.addEventListener('scroll', scrollActive )
